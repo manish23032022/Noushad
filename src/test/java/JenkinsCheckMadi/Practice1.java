11a842ce143833c6cd3f2a8660a83dbf117cf1ba
manish23032022 <102226602+manish23032022@users.noreply.github.com>
2024-05-31 23:08:14 +0530

    11:08

---
+++ src/test/java/JenkinsCheckMadi/Practice1.java
@@ -0,0 +1,30 @@
+package JenkinsCheckMadi;
+
+import org.testng.annotations.Test;
+
+public class Practice1 {
+    @Test
+	public void ATest() {
+		  System.out.println("Practiec first Smoke Testing");
+		
+	}
+    @Test
+   	public void BTest() {
+   		  System.out.println("Practiec first regression Testing");
+   	}
+    
+    @Test
+   	public void CTest() {
+   		  System.out.println("Changes Files  10:10");
+   	}
+    
+    @Test
+   	public void DTest() {
+   		  System.out.println("Changes Files --> 10:24");
+   	}
+    
+    @Test
+   	public void ETest() {
+    	  System.out.println("Changes Files -->  10:28");
+   	}
+}
---
+++ src/test/java/JenkinsCheckMadi/Practice2.java
@@ -0,0 +1,14 @@
+package JenkinsCheckMadi;
+
+import org.testng.annotations.Test;
+
+public class Practice2 {
+	@Test
+	public void Test1() {
+		  System.out.println("Practiec second Smoke Testing");
+	}
+    @Test
+   	public void Test2() {
+   		  System.out.println("Practiec second regression Testing");
+   	}
+}
